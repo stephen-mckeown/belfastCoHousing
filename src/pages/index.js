1d82ae34f9764b578a457d8997ac7fe91d595be6
smckeown <smckeown@spotx.tv>
2022-03-11 23:29:27 +0000

    more details

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,13 +83,18 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Landing Page</p>
-      <p>blab blab</p>
+      <p>We are Cohousing Belfast.</p>
+      <p>We believe in a future where housing is green and affordable.</p>
+      <StaticImage
+        alt="Belfast Brick Build Blue"
+        src="../images/belfastBlue.png"
+      />
     </Layout>
   )
 }
